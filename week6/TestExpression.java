4abe09d72c5730f2771858ad556222ff11e4f0a2
pipe34150 <pipe34150@hotmail.com>
2018-12-26 10:22:46 +0700

    Week 6

---
+++ week6/TestExpression.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sit.int102.week6;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author INT102
+ */
+public class TestExpression {
+
+    public static void main(String[] args) {
+        /*
+        boolean onOffStatus = true;
+
+        int result = 'A' + 1;
+        long studentId = 61130500095L;
+        float newId = studentId;
+        System.out.printf("Result = %d\n", result);
+        System.out.printf("Your Student id = %d , %f\n", studentId, newId);
+
+        final double TAX = 7f / 100;
+        int order = 1000;
+
+        double orderTax = order * TAX;
+
+        System.out.printf("Tax = %.2f\n", orderTax);
+        System.out.printf("Amount included tax = %.2f\n", orderTax + order);
+        
+        char[] allPlans ={ 'A', 'B', 'C' };
+        System.out.printf("%c\n",allPlans[2]);
+    
+        double farenheit;
+        double celcius = 25.0;
+        
+        farenheit = 9/5d*celcius + 32;
+        System.out.printf("BOOO %.2f",farenheit);
+    */    
+        Scanner sc = new Scanner(System.in);
+
+//        double x1,x2,y1,y2;
+//        System.out.println("Enter x1 : ");
+//        x1 = sc.nextDouble();
+//        System.out.println("Enter x2 : ");
+//        x2 = sc.nextDouble();
+//        System.out.println("Enter y1 : ");
+//        y1 = sc.nextDouble();
+//        System.out.println("Enter y1 : ");
+//        y2 = sc.nextDouble();
+//        double x = Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
+//        System.out.println(x);
+////        
+//        double a=1,b=1,c=1,d=1,e=1,f=1,g=1,h=1;
+//        
+//        double sum = ((1+2*a)/3) + ((4*(b+c)*(5-d-e))/f) - (6*(7/g+h));
+//        System.out.println(sum);
+//        
+//        int x=1;
+//        x+= ++x;
+//        System.out.println(x);
+
+
+          
+    }
+    
+}
